3bef4e6978bb7991744b0d0d1cbab4c60957d680
Jakub-Pazio <19jakubpazio99@gmail.com>
2024-01-17 01:19:49 +0100

    Minor css kifkas because its ugly

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Outlet, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {ManageUser} from "./components/ManageUser.tsx";
 import {UserAddForm} from "./components/UserAddForm.tsx";
@@ -9,16 +9,39 @@ import UsersList from "./components/UsersList.tsx";
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path={`/manage/:personalId/:accountType`} element={<ManageUser/>}/>
-                <Route path={`/rents/:accountId`} element={<ClientRents/>}/>
-                <Route path={'/addAccount'} element={<UserAddForm/>}/>
-                <Route path={'/addRent/:accountId'} element={<RentAddForm/>}/>
-                <Route path={'/usersList'} element={<UsersList/>}/>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path={`/manage/:personalId/:accountType`} element={<ManageUser/>}/>
+                    <Route path={`/rents/:accountId`} element={<ClientRents/>}/>
+                    <Route path={'/addAccount'} element={<UserAddForm/>}/>
+                    <Route path={'/addRent/:accountId'} element={<RentAddForm/>}/>
+                    <Route path={'/usersList'} element={<UsersList/>}/>
+                </Route>
             </Routes>
-        </Router>
+        </BrowserRouter>
+    );
+}
+
+function Layout() {
+    return (
+        <div className="App">
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={`/usersList`}>User List</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={'/addAccount'}>Add User</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
     );
 }
 
